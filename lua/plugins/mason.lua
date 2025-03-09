2eaf7cd720716af794d38f7a1553705882c6b3fb
1mper1os <p.pradog25@gmail.com>
2025-03-09 18:45:30 -0500

    r-mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,90 +1,61 @@
 local status_ok, mason = pcall(require, "mason")
-if not status_ok then
-	return
-end
+if not status_ok then return end
 
 local status_ok_lspconfig, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not status_ok_lspconfig then
-	return
-end
+if not status_ok_lspconfig then return end
 
 local status_ok_tool_installer, mason_tool_installer = pcall(require, "mason-tool-installer")
-if not status_ok_tool_installer then
-	return
-end
+if not status_ok_tool_installer then return end
 
 mason.setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-		border = "rounded",
-		keymaps = {
-			toggle_package_expand = "<CR>",
-			install_package = "i",
-			update_package = "u",
-			check_package_version = "c",
-			uninstall_package = "X",
-		},
-	},
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+    border = "none",  -
+    keymaps = {
+      toggle_package_expand = "<CR>",
+      install_package = "i",
+      update_package = "u",
+      check_package_version = "c",
+      uninstall_package = "X",
+    },
+  },
+  max_concurrent_installers = 5,  
 })
 
 mason_lspconfig.setup({
-	ensure_installed = {
-		"lua_ls",
-		"pyright",
-		"rust_analyzer",
-		"gopls",
-		"bashls",
-		"html",
-		"cssls",
-		"jsonls",
-		"yamlls",
-		"clangd",
-		"dockerls",
-		"graphql",
-		"jdtls",
-		"kotlin_language_server",
-		"marksman",
-		"prismals",
-		"sqlls",
-		"tailwindcss",
-		"terraformls",
-		"vimls",
-		"lemminx",
-		"zls",
-		"svelte",
-		"emmet_ls",
-		"eslint",
-		"denols",
-		"angularls",
-		"vuels",
-		"texlab",
-		"ltex",
-		"phpactor",
-		"intelephense",
-		"powershell_es",
-		"perlnavigator",
-		"elixirls",
-		"glsl_analyzer",
-		"unocss",
-		"wgsl_analyzer",
-	},
-	automatic_installation = true, -- Instala automáticamente los servidores LSP listados
+  ensure_installed = {
+    "lua_ls",
+    "pyright",
+    "rust_analyzer",
+    "gopls",
+    "bashls",
+    "html",
+    "cssls",
+    "jsonls",
+    "yamlls",
+    "clangd",
+    "dockerls",
+  },
+  automatic_installation = false, 
 })
 
--- Configuración de Mason-Tool-Installer
 mason_tool_installer.setup({
-	ensure_installed = {
-		"prettier",
-		"stylua",
-		"black",
-		"isort",
-		"eslint_d",
-		"shfmt",
-		"shellcheck",
-		"yamlfmt",
-	},
+  ensure_installed = {
+    "prettier",
+    "stylua",
+    "black",
+    "isort",
+    "eslint_d",
+    "shfmt",
+    "shellcheck",
+  },
+  auto_update = false,  
+  run_on_start = false, 
 })
+
+vim.g.mason_home = vim.fn.stdpath("data") .. "/mason"  
+vim.env.PATH = vim.env.PATH .. ":" .. vim.g.mason_home .. "/bin"  
